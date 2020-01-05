025831940ab018b18773ee24a9c095bd1529016e
jusola2 <juanjosesolano@hotmail.com>
2020-01-04 18:44:09 -0600

    Update activity.model.ts

--- src/models/activity.model.ts
+++ src/models/activity.model.ts
@@ -28,7 +28,7 @@ export class Activity extends Entity {
   })
   date: string;
 
-  @property({
+  @property({//esto representa a la sede donde se realiza la ubicacion
     type: 'string',
     required: true,
   })
