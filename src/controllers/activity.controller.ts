8ae56316af71f73988e1f6c0db1b26b0a5ec6167
jusola2 <juanjosesolano@hotmail.com>
2020-01-11 19:29:19 -0600

    Cambios en modelo Activity
    Tambien se agrego el paquete CRON para trabajos programados

--- src/controllers/activity.controller.ts
+++ src/controllers/activity.controller.ts
@@ -21,11 +21,31 @@ import { Activity } from '../models';
 import { ActivityRepository } from '../repositories';
 import { secured, SecuredType } from '../auth';
 
+const CronJob = require('cron').CronJob;
+
+console.log('Before job instantiation');
+//const job = new CronJob('00 00 00 * * *', function () {
+const job = new CronJob('* 01 * * * *', function () {
+  const d = new Date();
+  console.log('Midnight:', d);
+});
+console.log('After job instantiation');
+job.start();
+
 export class ActivityController {
   constructor(
     @repository(ActivityRepository)
     public activityRepository: ActivityRepository,
   ) { }
+
+  dateFromString = (date: string): Date => {
+    var pieces = date.split("/");
+    var day = parseInt(pieces[0])
+    var month = parseInt(pieces[1])
+    var year = parseInt(pieces[2])
+
+    return new Date(year, month, day)
+  }
 
   @post('/activities', {
     responses: {
@@ -84,7 +104,7 @@ export class ActivityController {
   async find(
     @param.query.object('filter', getFilterSchemaFor(Activity)) filter?: Filter<Activity>,
   ): Promise<Activity[]> {
-    return this.activityRepository.find(filter);
+    return this.activityRepository.find({ where: { state: 'Activo', date: { gte: new Date() } } });
   }
 
   @get('/activities/{id_user}', {
